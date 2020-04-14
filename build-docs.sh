6c28ffc0eb371560ac93d94d488e857a4f44134a
mastersan-mh <mastersan_mh@list.ru>
2020-04-14 11:50:17 +0500

    (refs #bitmap) docs build script refactored.

--- build-docs.sh
+++ build-docs.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
+check_app()
+{
+    APP=$1
+    STR=`whereis "${APP}" 2> /dev/null | sed "s/^${APP}://g"`
+    test -z "${STR}"
+    return $?
+}
+
 doxygen Doxyfile
+
+if check_app dot ; then
+    echo "[INFO]: 'dot' not found, so graphs not builded"
+    echo "[INFO]: Install on Ubuntu: apt install graphviz"
+fi
